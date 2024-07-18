6a1c982fff9c5b84d4533a528754c247c9a13145
BitCoin43 <rodion.logachev1423@mail.ru>
2024-07-18 20:41:52 +0000

    adsad

--- scr.js
+++ scr.js
@@ -1,5 +1,11 @@
 const db = require('./database').Products
 
-db.allOrders((err, data) => {
-    console.log(data)
+let data = {
+    name: "link",
+    value: "/img/products/13/0.png^/img/products/13/1.png^/img/products/13/2.png",
+    id: 13
+}
+db.update(data, (err) => {
+    if (err) console.log(err)
+    else console.log("nice")
 })
